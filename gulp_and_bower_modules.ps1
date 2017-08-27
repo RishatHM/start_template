35ea41df6bc6a524b652abefa0189caabfd73511
Rishat Haydarshin <rishathm@gmail.com>
2017-08-27 14:31:10 +0500

    Add files via upload

---
+++ gulp_and_bower_modules.ps1
@@ -0,0 +1,15 @@
+npm i gulp --save-dev
+npm i gulp-util --save-dev
+npm i gulp-watch --save-dev
+npm i gulp-autoprefixer --save-dev 
+npm i gulp-uglify --save-dev
+npm i gulp-sass --save-dev
+npm i gulp-sourcemaps --save-dev
+npm i gulp-rigger --save-dev
+npm i gulp-minify-css --save-dev
+npm i gulp-imagemin --save-dev
+npm i imagemin-pngquant --save-dev
+npm i rimraf --save-dev
+npm i vinyl-ftp --save-dev
+npm i browser-sync --save-dev
+bower i
